abb3014350edb17addc95298a12390c4a541754d
Gaurav Gupta <windgg100@gmail.com>
2016-09-09 00:03:47 +0530

    changed jna access and crated a calling class for behaviour model

---
+++ ml/src/main/scala/ml/BehaviourModel.scala
@@ -0,0 +1,28 @@
+package aianash.ml
+
+import com.sun.jna.{Library, Native}
+
+
+private trait BehaviourJNA extends Library {
+
+  def aianash_behaviourTrain(inputFilePath: String, outputFilePath: String)
+
+}
+
+
+private object BehaviourJNA {
+
+  val instance = Native.loadLibrary("libaianash", classOf[BehaviourJNA]).asInstanceOf[BehaviourJNA]
+
+}
+
+
+class BehaviourModel {
+
+  import BehaviourJNA.instance
+
+  def train(inputFilePath: String, outputFilePath: String) {
+    instance.aianash_behaviourTrain(inputFilePath, outputFilePath)
+  }
+
+}
